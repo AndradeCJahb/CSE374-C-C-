35e916ed5dd292e0b47dc191215d4fddd3f9062b
Christopher Andrade <andradec@uw.edu>
2023-11-27 01:49:34 -0800

    hw5

---
+++ HW5/tnine.c
@@ -0,0 +1,105 @@
+/* tnine is a program that drives a trie / t9 program.  This code
+   will build a trie, according to trienode.  It will also run
+   an interactive session where the user can retrieve words using
+   t9 sequences.
+   CSE374, HW5, 22wi
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "trienode.h"
+
+
+void run_session(trieNode *wordTrie);
+
+int main(int argc, char **argv) {
+    FILE *dictionary = NULL;
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s [DICTIONARY]\n", argv[0]);
+        return EXIT_FAILURE;
+    } else {
+        dictionary = fopen(argv[1], "r");
+        if (!dictionary) {
+            fprintf(stderr, "Error: Cannot open %s\n", argv[1]);
+            return EXIT_FAILURE;
+        }
+    }
+
+    trieNode* root = build_trie(dictionary);
+
+    run_session(root);
+
+    free_tree(root);
+
+    fclose(dictionary);
+
+    return(EXIT_SUCCESS);
+}
+
+trieNode* build_trie(FILE *dict) {
+    trieNode* root = createTrieNode();
+    if (!root) {
+        perror("Error creating TrieNode");
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[MAXLEN + 1];
+
+    while (fgets(buffer, sizeof(buffer), dict) != NULL) {
+        buffer[strcspn(buffer, "\n")] = '\0';
+        insertWord(root, buffer);
+    }
+
+    return root;
+}
+
+void run_session(trieNode *wordTrie) {
+    printf("Enter \"exit\" to quit.\n");
+
+    char input[MAXLEN];
+    char *prevInput = (char *)malloc(52);
+    if (prevInput == NULL) {
+        perror("Error allocating memory for prevWord");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        printf("Enter Key Sequence (or \"#\" for next word):\n");
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            break;
+        }
+
+        input[strcspn(input, "\n")] = '\0';
+
+        if (strcmp(input, "exit") == 0) {
+            break;
+        }
+
+        char *word = NULL;
+
+        if (strcmp(input, "#") != 0) {
+            word = get_word(wordTrie, input);
+
+        } else {
+            if (prevInput == NULL) {
+                printf("There are no more T9onyms\n");
+            } else {
+                word = get_word(wordTrie, strcat(prevInput, "#"));
+            }
+        }
+
+        strcpy(prevInput, input);
+
+        if (word != NULL) {
+            printf("'%s'\n", word);
+        } else if (strchr(input, '#')) {
+            printf("There are no more T9onyms\n");
+        } else {
+            printf("Not found in the current dictionary.\n");
+        }
+    }
+
+    free(prevInput);
+}
---
+++ HW5/trie.c
@@ -0,0 +1,139 @@
+/* trie implements a trie, made of trieNodes. This includes
+   code to build, search, and delete a trie
+   CSE374, HW5, 22wi
+*/
+
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include "trienode.h"
+
+char* translate(const char* word);
+
+void insertWord(trieNode* root, const char* word) {
+    trieNode* current = root;
+    char* wordNum  = translate(word);
+
+    for (int i = 0; i < strlen(wordNum); i++) {
+        char currNum = wordNum[i];
+        if (current->branches[currNum - '2'] == NULL) {
+            current->branches[currNum - '2'] = createTrieNode();
+        }
+        current = current->branches[currNum - '2'];
+    }
+
+    free(wordNum);
+    if (current->word == NULL) {
+        current->word = (char*)malloc(strlen(word) +1);
+        if (current->word == NULL) {
+            perror("Error copying words into trie");
+            exit(EXIT_FAILURE);
+        }
+        strcpy(current->word, word);
+    }
+    else
+    {
+        while (current->word != NULL) {
+            if (current->branches[BRANCHES - 1] == NULL) {
+                current->branches[BRANCHES - 1] = createTrieNode();
+            }
+            current = current->branches[BRANCHES - 1];
+        }
+        current->word = (char*)malloc(strlen(word) + 1);
+        if (current->word == NULL) {
+            perror("Error copying words into trie");
+            exit(EXIT_FAILURE);
+        }
+        strcpy(current->word, word);
+    }
+}
+
+char* get_word(trieNode *root, char *pattern) {
+    trieNode *current = root;
+
+    for (size_t i = 0; i < strlen(translate(pattern)); i++) {
+        char currentChar = pattern[i];
+
+        int index;
+        if (currentChar == '#') {
+            index = BRANCHES - 1;
+        } else {
+            index = currentChar - '2';
+        }
+
+        if (current->branches[index] == NULL) {
+            return NULL;
+        }
+
+        current = current->branches[index];
+    }
+
+    if (current->word != NULL) {
+        return current->word;
+    } else {
+        return NULL;
+    }
+}
+
+char* translate(const char* word) {
+    char* t9Sequence = (char*)malloc((strlen(word) + 1));
+
+    if (t9Sequence == NULL) {
+        perror("Error allocating memory for T9 translation");
+        exit(EXIT_FAILURE);
+    }
+
+    for (size_t i = 0; i < strlen(word); i++) {
+        char currentChar = tolower(word[i]);
+
+        if (currentChar >= 'a' && currentChar <= 'c') {
+            t9Sequence[i] = '2';
+        } else if (currentChar >= 'd' && currentChar <= 'f') {
+            t9Sequence[i] = '3';
+        } else if (currentChar >= 'g' && currentChar <= 'i') {
+            t9Sequence[i] = '4';
+        } else if (currentChar >= 'j' && currentChar <= 'l') {
+            t9Sequence[i] = '5';
+        } else if (currentChar >= 'm' && currentChar <= 'o') {
+            t9Sequence[i] = '6';
+        } else if (currentChar >= 'p' && currentChar <= 's') {
+            t9Sequence[i] = '7';
+        } else if (currentChar >= 't' && currentChar <= 'v') {
+            t9Sequence[i] = '8';
+        } else {
+            t9Sequence[i] = '9';
+        }
+    }
+
+    t9Sequence[strlen(word)] = '\0';
+    return t9Sequence;
+}
+
+trieNode* createTrieNode() {
+    trieNode* node = (trieNode*)malloc(sizeof(trieNode));
+    if (node == NULL) {
+        perror("Error creating TrieNode");
+        exit(EXIT_FAILURE);
+    }
+    for (int i = 0; i < BRANCHES; i++) {
+        node->branches[i] = NULL;
+    }
+
+    node->word = NULL;
+
+    return node;
+}
+
+void free_tree(trieNode* root) {
+    if (root == NULL) {
+        return;
+    }
+
+    for (int i = 0; i < BRANCHES; ++i) {
+        free_tree(root->branches[i]);
+    }
+
+    free(root->word); // Free the word associated with the node
+    free(root);       // Free the node itself
+}
