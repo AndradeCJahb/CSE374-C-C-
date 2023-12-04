0c18b77879b5683056e91233d0be81df844ce1ad
Christopher Andrade <andradec@uw.edu>
2023-12-03 23:16:28 -0800

    hw6a final

--- HW6A/bench.c
+++ HW6A/bench.c
@@ -1,5 +1,8 @@
-/* bench.c is the benchmarking /test program for mem memory management
-   bench.c is implemented as part of HW6, CSE374 22WI
+/*
+  Christopher Andrade
+  12/3/2023
+  bench.c
+  bench.c is the benchmarking /test program for mem memory management
 */
 
 #include <stdlib.h>
@@ -36,6 +39,8 @@ int main(int argc, char** argv ) {
     int large_limit = 20000;
     int random_seed = (int)time(NULL);
 
+    // Edits any of the many parameters accordingly if command line contains
+    // further arguments.
     if (argc > 1) ntrials = atoi(argv[1]);
     if (argc > 2) pctget = atoi(argv[2]);
     if (argc > 3) pctlarge = atoi(argv[3]);
@@ -45,13 +50,15 @@ int main(int argc, char** argv ) {
 
     srand(random_seed);
 
-
     void* blocks[ntrials];
     int nblocks = 0;
     clock_t start = clock();
 
     for (int i = 0; i < ntrials; i++) {
         if ((rand() % 100) < pctget) {
+            // creates a new block of memory, either large or small which
+            // depends on possibly inputted limits to block size and percent
+            // chance of utilizing small or large limit.
             uintptr_t size;
             if ((rand() % 100) < pctlarge) {
                 size = small_limit + (rand() % (large_limit - small_limit+ 1));
@@ -59,6 +66,7 @@ int main(int argc, char** argv ) {
                 size = 1 + (rand() % small_limit);
             }
 
+            // Newly created block of data is populated.
             blocks[nblocks] = getmem(size);
             if (blocks[nblocks] != NULL) {
                 fill_mem(blocks[nblocks], size);
@@ -66,6 +74,8 @@ int main(int argc, char** argv ) {
                 nblocks++;
             }
         } else {
+            // Frees a random block of memory, given at least 1 block
+            // has been created.
             if (nblocks > 0) {
                 int idx = rand() % nblocks;
                 freemem(blocks[idx]);
@@ -74,8 +84,10 @@ int main(int argc, char** argv ) {
                 nblocks--;
             }
         }
-        if(i > 10)
-        {
+
+        // Prints only at roughly 10% increments of the number of trials
+        // does not print at increments if number of trials is less than 10.
+        if(i > 9) {
             if (i % (ntrials / 10) == 0) {
                 print_stats(start);
             }
@@ -87,16 +99,17 @@ int main(int argc, char** argv ) {
 }
 
 void print_stats(clock_t start) {
+    // Calculates the amount of time passed.
     clock_t end = clock();
     double elapsed_time = ((double) (end - start)) / CLOCKS_PER_SEC;
 
+    // Statistics about the memory bench which will be outputted.
     uintptr_t total_size, total_free, n_free_blocks;
     get_mem_stats(&total_size, &total_free, &n_free_blocks);
-
     double avg_bytes = (double) total_free / n_free_blocks;
 
-    printf("Elapsed Time: %.6f seconds | Total Size: %lu bytes | "
-           "Total Free Blocks: %lu | Avg Bytes: %.2f\n",
+    printf("Elapsed CPU Time: %.6f seconds | Total Size: %lu bytes | "
+           "Total Free Blocks: %lu | Avg Bytes in Free Blocks: %.2f\n",
            elapsed_time, total_size, n_free_blocks, avg_bytes);
 }
 
