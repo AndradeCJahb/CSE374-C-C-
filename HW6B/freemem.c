03bc08c72c99fef8151212aa3e640e033fb01483
Christopher Andrade <andradec@uw.edu>
2023-12-13 07:29:24 -0800

    hw6bfinal6

--- HW6B/freemem.c
+++ HW6B/freemem.c
@@ -10,8 +10,11 @@
 #include "mem.h"
 #include "mem_impl.h"
 
+// Combines adjacent free memory chunks
 void combine();
 
+// Returns the given memory chunk back to the freelist to classify it
+// as a free memory chunk.
 void freemem(void* p) {
     check_heap();
 
@@ -23,19 +26,24 @@ void freemem(void* p) {
     uintptr_t chunkSize = chunk->size;
     totalmalloc = totalmalloc - chunkSize - NODESIZE;
 
-    // Add the block to the free list and sort by memory address
+    // Initializes pointers required to loop through freelist.
     freeNode* current = freelist;
     freeNode* prev = NULL;
 
+    // Iterates through freelist until arrived at address which is to be
+    // freed, or end of freelist.
     while (current != NULL && current < chunk) {
         prev = current;
         current = current->next;
     }
 
+    // Ensures no memory is freed if given address is not a chunk of memory
+    // that hasn't been allocated.
     if (current == NULL) {
         return;
     }
 
+    // Fixes pointers to add chunk of memory back into freelist.
     if (prev == NULL) {
         freelist = chunk;
     } else {
@@ -50,15 +58,19 @@ void freemem(void* p) {
 
 void combine()
 {
+    // Loops through each node within freelist to ensure that there are no
+    // two free memory chunks directly adjacent to one another.
     freeNode* current = freelist;
     while (current != NULL && current->next != NULL) {
+        // Checks if address of next chunk is the same as the current chunk
+        // address + its own size.
         if ((uintptr_t)current + NODESIZE + current->size ==
             (uintptr_t)current->next) {
+
             // Combine the current block with the next one
             current->size = current->size + NODESIZE + current->next->size;
             current->next = current->next->next;
         } else {
-            // Move to the next block
             current = current->next;
         }
     }
--- HW6B/getmem.c
+++ HW6B/getmem.c
@@ -16,29 +16,34 @@
 freeNode* freelist = NULL;
 uintptr_t totalmalloc = 0;
 
+// Splits given node into two pieces, in the case that the proposed memory
+// chunk is large enough to fit the smallest memory chunk.
 void split_node(freeNode* n, uintptr_t size);
 
+// Allocates and returns a pointer to allocated memory. The size of the
+// returned memory chunk will be the parameter 'size' rounded to its
+// next multiple of 16.
 void* getmem(uintptr_t size) {
-    // make sure you return a pointer to the usable memory that
-    // is at least 'size' bytes long.
-    // To get you started we are 'stubbing in' a call that will
-    // return a usable value.  You will replace this code.
     check_heap();
 
-    // Ensure that size is positive
+    // Ensure that size is positive.
     if (size < 1) {
         return NULL;
     }
 
     uintptr_t totalSize = size + NODESIZE;
 
+    // Ensures the total size allocated is divisible by 16.
     if (totalSize % 16 != 0) {
         totalSize = totalSize - (totalSize % 16) + 16;
     }
 
+    // Initializes pointers to iterate through freelist.
     freeNode* current = freelist;
     freeNode* prev = NULL;
 
+    // loops through freelist until a big enough free chunk is found or until end
+    // of freelist.
     while (current != NULL && current->size < totalSize) {
         prev = current;
         current = current->next;
@@ -46,6 +51,8 @@ void* getmem(uintptr_t size) {
 
     if (current == NULL) {
         if (BIGCHUNK + NODESIZE < totalSize) {
+            // Creates new chunk which is larger than default big chunk size.
+            // Splitting of newly created chunk is done if needed.
             current = (freeNode*)malloc(totalSize);
             if (current == NULL) {
                 return NULL;
@@ -54,6 +61,8 @@ void* getmem(uintptr_t size) {
             current->size = totalSize - NODESIZE;
             current->next = NULL;
         } else {
+            // Creates new chunk which is the size of big chunk. Splitting
+            // of newly created chunk is done if needed.
             current = (freeNode*)malloc(BIGCHUNK + NODESIZE);
             if (current == NULL) {
                 return NULL;
@@ -66,6 +75,8 @@ void* getmem(uintptr_t size) {
             freelist = current->next;
         }
     } else {
+        // If chunk of sufficient size is found then it is allocated. Splitting
+        // of existing chunk is done if needed.
         if (current->size > totalSize + MINCHUNK + NODESIZE) {
             split_node(current, totalSize);
         }
@@ -76,14 +87,20 @@ void* getmem(uintptr_t size) {
         }
     }
 
+    // Updates totalmalloc statistic which is the total size of allocated
+    // chunks of memory.
     totalmalloc = totalmalloc + totalSize;
     return ((void*)current + NODESIZE);
 }
 
 void split_node(freeNode* n, uintptr_t totalSize) {
     freeNode* newNode = (freeNode*)((uintptr_t)(n) + totalSize);
+
+    // fields of new node "ahead" of original are updated appropriately
     newNode->size = n->size - totalSize;
     newNode->next = n->next;
+
+    // fields of original node are updated appropriately
     n->size = totalSize - NODESIZE;
     n->next = newNode;
 }
